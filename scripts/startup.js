dd2e9b8a5c4b434fa4f2c164a93376f57d577562
lowrybg <lowrybg@gmail.com>
2017-10-04 14:54:28 +0300

    category creation add button linked

--- scripts/startup.js
+++ scripts/startup.js
@@ -1,15 +1,26 @@
+
 $(function(){
     const categoryName = $('#category-name');
     const taskName = $('#task-name');
     const taskDescription = $('#task-description');
-
+    
     const board = $('#board');
-
+    $('p').attr("id","paragraph");
     $('#create-category').on('click',() => {
         container.categoryAct.addCategory(categoryName.val());
-        board.append('<div>'+ categoryName.val() +'</div>');
-        categoryName.val('');
+        document.getElementById("paragraph").innerHTML=categoryName.val();
+        document.getElementById("category-name").value = "";
+
+       
     })
+    
+    // $(document).ready(function(){
+       
+    //     $("#create-category").click(function(){
+    //         $("p").text(('#category-name').value);
+           
+    //     });
+    // });
     //TODO
     //Deleting the div where is the task
     $('#delete-category').on('click',() => {
