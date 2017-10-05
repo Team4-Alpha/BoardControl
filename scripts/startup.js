1f8b3b661c838526559671785c74467470b1007f
lowrybg <lowrybg@gmail.com>
2017-10-05 19:52:35 +0300

    category notes creation partial

--- scripts/startup.js
+++ scripts/startup.js
@@ -4,25 +4,22 @@ $(function(){
     const taskName = $('#task-name');
     const taskDescription = $('#task-description');
     
-    const board = $('#board');
-    $('p').attr("id","paragraph");
+
+
     $('#create-category').on('click',() => {
         container.categoryAct.addCategory(categoryName.val());
-        document.getElementById("paragraph").innerHTML=categoryName.val();
-        document.getElementById("category-name").value = "";
-
+        
+        $(`<div id="myDIV" class="header">
+                <h2>${categoryName.val()}</h2>
+                <input type="text" id="myInput" placeholder="Title...">
+                <span onclick="newElement()" class="addBtn">Add</span>
+           </div>
+         <ul id="myUL">
+            </ul>`).appendTo('#wrapper');
+            document.getElementById("category-name").value="";
        
     })
     
-    // $(document).ready(function(){
-       
-    //     $("#create-category").click(function(){
-    //         $("p").text(('#category-name').value);
-           
-    //     });
-    // });
-    //TODO
-    //Deleting the div where is the task
     $('#delete-category').on('click',() => {
         container.categoryAct.deleteCategory();
         
